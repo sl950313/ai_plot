55bac6ccdd3f51967e0193403413e90ec4187867
本初 <shilei3@xiaohongshu.com>
2024-07-31 20:03:50 +0800

    daily work ended_v2.

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,23 @@
 // index.js
-Page({})
+Page({
+  data: {
+    
+  },
+
+  toNormalDraw: function() {
+    wx.navigateTo({
+      url: '/pages/draw/draw',
+    });
+  },
+
+  toPicDraw: function() {
+    wx.navigateTo({
+      url: '/pages/draw/pic_draw',
+    })
+  },
+
+  onLoad: function () {
+    // const ctx = wx.createCanvasContext('myCanvas');
+    // this.setData({ ctx });
+  }
+})
